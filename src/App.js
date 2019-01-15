7300f73cd2b7f6c856bc8da4ff26f17d0b0a6b23
kittist <kittisq@gmail.com>
2019-01-15 21:22:47 +0700

    add router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom"
 import MainContainer from './containers/main'
 
 class App extends Component {
@@ -6,7 +7,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <MainContainer />
+         <Router>
+            <div>  
+              <Route exact path="/" component={MainContainer} />
+            </div>
+          </Router>
       </div>
     );
   }
